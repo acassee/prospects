3e730da6913703dfdcad7a8df308c3ea4027bd49
acassee <adcassee@outlook.com>
2025-02-06 21:07:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-const players = ["Connor Bedard", "Logan Cooley", "Adam Fantilli"];
-
-const list = document.getElementById("player-list");
-players.forEach(player => {
-    let listItem = document.createElement("li");
-    listItem.textContent = player;
-    list.appendChild(listItem);
+document.addEventListener('DOMContentLoaded', function () {
+    alert('JavaScript is working!');
 });
